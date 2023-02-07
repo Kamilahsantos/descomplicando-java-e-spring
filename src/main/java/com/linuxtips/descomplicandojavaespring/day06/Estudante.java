b0e151aa52a3b355ca720e1ae8ca2c6cf51617cd
Kamila Santos <kahsantos89@gmail.com>
2023-02-06 21:09:08 -0300

    dia 06

---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day06/Estudante.java
@@ -0,0 +1,27 @@
+package com.linuxtips.descomplicandojavaespring.day06;
+
+public class Estudante {
+
+    private String nome;
+    private String stack;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStack() {
+        return stack;
+    }
+
+    public void setStack(String stack) {
+        this.stack = stack;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day06/Objetos.java
@@ -0,0 +1,17 @@
+package com.linuxtips.descomplicandojavaespring.day06;
+
+public class Objetos {
+
+    public static void main(String[] args) {
+
+
+        Estudante kamilaCode = new Estudante();
+        kamilaCode.setNome("Kamila");
+        kamilaCode.setStack("java e Spring");
+        System.out.println(kamilaCode.getNome());
+        System.out.println(kamilaCode.getStack());
+
+
+    }
+}
+
