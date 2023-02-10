5157928c7587fd16383f730f0ff34e46c783929a
Kamila Santos <kahsantos89@gmail.com>
2023-02-09 23:49:12 -0300

    dia 08: scanner

---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day08/PedidoScanner.java
@@ -0,0 +1,28 @@
+package com.linuxtips.descomplicandojavaespring.day08;
+
+import java.util.Scanner;
+
+public class PedidoScanner {
+
+    public static void main(String[] args) {
+
+        System.out.println("Por favor, insira o código do produto");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String codigo = scanner.nextLine();
+        System.out.println("Por favor, insira a quantidade de itens desse produto");
+        Integer quantidade = scanner.nextInt();
+
+
+        System.out.println("Digite o valor do produto");
+
+        Double valor = scanner.nextDouble();
+
+        Double total = valor * quantidade;
+
+        System.out.println("O valor total da sua compra é  " + total);
+
+
+    }
+}
