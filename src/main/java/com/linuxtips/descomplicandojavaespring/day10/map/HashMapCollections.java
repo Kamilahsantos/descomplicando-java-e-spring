ed8722ca5d242bba7299eab64cce3486921f70ee
Kamila Santos <kahsantos89@gmail.com>
2023-02-18 22:56:27 -0300

    dia 10: map

--- src/main/java/com/linuxtips/descomplicandojavaespring/day10/map/HashMapCollections.java
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day10/map/HashMapCollections.java
@@ -7,24 +7,11 @@ public class HashMapCollections {
     public static void main(String[] args) {
 
         //armazena chave valor
-        //chave -> indicide
+        //chave -> indice
         //objeto -> valor
         //permite chaves nulas
         //nao garante a ordem
 
-        HashMap<String, Double> notas = new HashMap<String, Double>();
-        notas.put("Lógica de programacao", 7.8);
-        notas.put("Orientacao a objetos", 9.5);
-        notas.put("Banco de dados", 7.2);
-        System.out.println(notas);
-
-
-        System.out.println(notas.get("Banco de dados"));
-        System.out.println(notas.getOrDefault("Banco do dados", 0.0));
-        System.out.println(notas.put("Java", 10.0));
-        System.out.println(notas);
-        System.out.println(notas.containsKey("Java"));
-        System.out.println(notas.containsValue(6.0));
 
 
 
