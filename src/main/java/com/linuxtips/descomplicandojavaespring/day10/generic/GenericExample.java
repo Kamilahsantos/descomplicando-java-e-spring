6f315cb815446043d339f4275ff2e7da21c722f5
Kamila Santos <kahsantos89@gmail.com>
2023-02-19 01:42:34 -0300

    dia 10: exemplo generics

--- src/main/java/com/linuxtips/descomplicandojavaespring/day10/generic/GenericExample.java
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day10/generic/GenericExample.java
@@ -1,5 +1,8 @@
 package com.linuxtips.descomplicandojavaespring.day10.generic;
 
+// vieram para facilitar principalmente casos de casting entre tipos
+//reduzindo a complexidade e volume de código escrito dados
+//  podemos usar um tipo genérico de dados.
 public class GenericExample <T>{
     public GenericExample(T mensagem) {
         this.name = mensagem;
