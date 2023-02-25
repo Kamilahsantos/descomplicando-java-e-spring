edd230d7de818761efd290268153d8d8bc782aec
Kamila Santos <kahsantos89@gmail.com>
2023-02-25 20:08:11 -0300

    day 14: lambda

---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day14/lambda/LambdaExample.java
@@ -0,0 +1,22 @@
+package com.linuxtips.descomplicandojavaespring.day14.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LambdaExample {
+
+    public static void main(String[] args) {
+        //pequeno bloco de codigo que recebe parametros e retorna um valor
+        //sao semelhantes aos metodos
+        // mas nao precisam de um nome e pode estar dentro do corpo de um outro metodo
+
+        List<String> curso = Arrays.asList("Java", "Linux", "Tips", "Spring");
+        List<String> terminaComS = curso.stream().filter(s -> s.equals("Jaca")).
+                collect(Collectors.toList());
+        System.out.println(terminaComS);
+
+    }
+
+}
