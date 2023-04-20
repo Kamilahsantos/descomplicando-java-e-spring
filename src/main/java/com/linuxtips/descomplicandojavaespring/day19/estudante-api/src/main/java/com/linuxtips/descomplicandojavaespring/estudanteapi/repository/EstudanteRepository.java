99e14f4dc758764fea448a2fec509f43b49c30f5
Kamila Santos <kahsantos89@gmail.com>
2023-04-20 00:44:59 -0300

    repository

---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day19/estudante-api/src/main/java/com/linuxtips/descomplicandojavaespring/estudanteapi/repository/EstudanteRepository.java
@@ -0,0 +1,10 @@
+package src.main.java.com.linuxtips.descomplicandojavaespring.estudanteapi.repository;
+
+
+import com.linuxtips.descomplicandojavaespring.estudanteapi.model.Estudante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstudanteRepository extends JpaRepository<Estudante,Long> {
+}
