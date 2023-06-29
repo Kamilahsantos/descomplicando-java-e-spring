978890487b5fdc5ab941cacfd55a701e65ec4f23
Kamila Santos <kahsantos89@gmail.com>
2023-06-28 23:08:31 -0300

    day 23: teste da repository

---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day23/estudante-api/src/main/java/com/linuxtips/descomplicandojavaespring/estudanteapi/controller/EstudanteController.java
@@ -0,0 +1,112 @@
+package com.linuxtips.descomplicandojavaespring.estudanteapi.controller;
+
+
+import com.linuxtips.descomplicandojavaespring.estudanteapi.model.Estudante;
+import com.linuxtips.descomplicandojavaespring.estudanteapi.service.EstudanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1")
+public class EstudanteController {
+
+    @Autowired
+    private EstudanteService estudanteService;
+
+
+
+    @PostMapping("/estudantes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Estudante criarEstudante(@RequestBody Estudante estudante) throws Exception {
+        return estudanteService.criarEstudante(estudante);
+    }
+
+
+    @GetMapping("/estudantes")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Estudante> listarEstudantes() {
+        return estudanteService.listarEstudantes();
+    }
+
+
+    @GetMapping("/estudantes/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Estudante> buscarEstudantePeloId
+            (@PathVariable(value = "id") Long id) {
+        return estudanteService.buscarEstudantePeloId(id);
+
+    }
+
+
+
+    @PutMapping("/estudantes/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Estudante> atualizarEstudantePeloId
+            (@PathVariable(value = "id") Long id,
+             @RequestBody Estudante estudante) {
+        return estudanteService.atualizarEstudantePeloId(estudante, id);
+
+    }
+
+
+    @DeleteMapping("/estudantes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> excluirEstudantePeloId
+            (@PathVariable(value = "id") Long id) {
+        return estudanteService.excluirEstudantePeloId(id);
+
+    }
+
+    @GetMapping("/estudantes/nome/{nome}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Estudante> buscarEstudantePeloNome
+            (@PathVariable(value = "nome") String nome) {
+        return estudanteService.buscarEstudantePeloNome(nome);
+
+    }
+
+    @GetMapping("/estudantes/curso")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Estudante> listarEstudantesPeloCurso(@RequestParam String nome_curso) {
+        return estudanteService.listarEstudantesPeloCurso(nome_curso);
+    }
+
+
+
+    @GetMapping("/estudantes/nome")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Estudante> listarEstudantesPeloComecoNome(@RequestParam String comeco_nome) {
+        return estudanteService.listarEstudantesPeloComecoDoNome(comeco_nome);
+    }
+
+    @GetMapping("/estudantes/curso-nome")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Estudante> listarEstudantesPeloComecoNomeECurso
+            (@RequestParam String comeco_nome,
+             @RequestParam String curso) {
+        return estudanteService.listarEstudantesPeloComecoDoNomeECurso(comeco_nome, curso);
+    }
+
+    @GetMapping("/estudantes/endereco")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Estudante> listarEstudantesPeloEndereco
+            (@RequestParam String endereco
+            ) {
+        return estudanteService.listarEstudantesPeloEndereco(endereco);
+    }
+
+    @GetMapping("/estudantes/promo")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Estudante> listarPrimeirosEstudantes
+            (@RequestParam Long id
+            ) {
+        return estudanteService.listarPrimeirosEstudantes(id);
+    }
+
+
+
+}
---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day23/estudante-api/src/main/java/com/linuxtips/descomplicandojavaespring/estudanteapi/model/Estudante.java
@@ -0,0 +1,97 @@
+package com.linuxtips.descomplicandojavaespring.estudanteapi.model;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "estudante")
+public class Estudante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String nome;
+    @Column(nullable = false, length = 10)
+    private String endereco;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "dados_bancarios_id", referencedColumnName = "id")
+    @JsonManagedReference
+    private DadosBancarios dadosBancarios;
+    @Column(nullable = false)
+    private String curso;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEm;
+
+
+    @UpdateTimestamp
+    @Column(name = "atualizado_em")
+    private  LocalDateTime atualizadoEm;
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public void setAtualizadoEm(LocalDateTime atualizadoEm) {
+        this.atualizadoEm = atualizadoEm;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public DadosBancarios getDadosBancarios() {
+        return dadosBancarios;
+    }
+
+    public void setDadosBancarios(DadosBancarios dadosBancarios) {
+        this.dadosBancarios = dadosBancarios;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+
+}
