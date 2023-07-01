523839ba2126ca35bda34abf21d0f232eeb846eb
Kamila Santos <kahsantos89@gmail.com>
2023-07-01 16:31:31 -0300

    day 23: teste da repository

--- src/main/java/com/linuxtips/descomplicandojavaespring/day23/estudante-api/src/test/java/com/linuxtips/descomplicandojavaespring/estudanteapi/EstudanteRepositoryTest.java
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day23/estudante-api/src/test/java/com/linuxtips/descomplicandojavaespring/estudanteapi/EstudanteRepositoryTest.java
@@ -5,6 +5,7 @@ import com.linuxtips.descomplicandojavaespring.estudanteapi.model.DadosBancarios
 import com.linuxtips.descomplicandojavaespring.estudanteapi.model.Estudante;
 import com.linuxtips.descomplicandojavaespring.estudanteapi.model.enums.TipoContaBancaria;
 import com.linuxtips.descomplicandojavaespring.estudanteapi.repository.EstudanteRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -22,49 +23,74 @@ public class EstudanteRepositoryTest {
 
 
     @Test
-    void DeveSalvarEstudanteNaBase() {
-        Estudante estudante = new Estudante();
-        DadosBancarios dadosBancarios = new DadosBancarios();
-        dadosBancarios.setTipoContaBancaria(TipoContaBancaria.valueOf("CORRENTE"));
-        dadosBancarios.setDigito(1);
-        dadosBancarios.setAgencia(1234);
-        dadosBancarios.setConta(123);
-        estudante.setNome("aluno");
-        estudante.setCurso("java");
-        estudante.setEndereco("rua");
-        estudante.setDadosBancarios(dadosBancarios);
+    @DisplayName("Sucesso - Deve salvar estudante na base com sucesso")
+    void DeveSalvarEstudanteNaBaseComSucesso() {
 
-        Estudante estudanteSalvo = this.estudanteRepository.save(estudante);
+        Estudante estudanteSalvo = this.estudanteRepository.save(buildMockEstudante());
         assertNotNull(estudanteSalvo);
         assertEquals("java", estudanteSalvo.getCurso());
     }
 
+
+    @DisplayName("Sucesso - Deve listar todos os estudantes na base com sucesso")
     @Test
-    void deveRetornarListaDeEstudantes() {
+    void deveRetornarListaDeEstudantesCadastradosNaBaseComSucesso() {
 
-        Estudante estudante = new Estudante();
-        DadosBancarios dadosBancarios = new DadosBancarios();
-        dadosBancarios.setTipoContaBancaria(TipoContaBancaria.valueOf("CORRENTE"));
-        dadosBancarios.setDigito(1);
-        dadosBancarios.setAgencia(1234);
-        dadosBancarios.setConta(123);
-        estudante.setNome("aluno");
-        estudante.setCurso("java");
-        estudante.setEndereco("rua");
-        estudante.setDadosBancarios(dadosBancarios);
-
-        estudanteRepository.save(estudante);
-
+        estudanteRepository.save(buildMockEstudante());
 
         List<Estudante> estudantes = this.estudanteRepository.findAll();
         assertEquals(1, estudantes.size());
     }
 
+
+    @DisplayName("Sucesso - Deve excluir um estudante com sucesso")
     @Test
-    void deveExcluirumEstudante() {
+    void deveExcluirUmEstudanteComSucesso() {
         this.estudanteRepository.deleteById(1L);
         Optional<Estudante> estudante = this.estudanteRepository.findById(1L);
         assertFalse(estudante.isPresent());
     }
 
+
+    @DisplayName("Sucesso - Deve buscar um estudante pelo id com sucesso")
+    @Test
+    void deveBuscarUmEstudantePeloIdComSucesso() {
+        estudanteRepository.save(buildMockEstudante());
+
+        Optional<Estudante> estudante = this.estudanteRepository.findById(1L);
+        assertTrue(estudante.isPresent());
+    }
+
+    @DisplayName("Sucesso - Deve buscar um estudante pelo nome com sucesso")
+    @Test
+    void deveBuscarUmEstudantePeloNomeComSucesso() {
+        estudanteRepository.save(buildMockEstudante());
+
+        Optional<Estudante> estudante = this.estudanteRepository.findByName("aluno");
+        assertTrue(estudante.isPresent());
+    }
+
+    @DisplayName("Sucesso - Deve buscar um estudante pelo nome e curso com sucesso")
+    @Test
+    void deveBuscarUmEstudantePeloNomeECursoComSucesso() {
+        estudanteRepository.save(buildMockEstudante());
+
+        List<Estudante> estudantes = this.estudanteRepository.findByNomeStartingWithAndCurso("a", "java");
+        assertEquals(1, estudantes.size());
+    }
+
+    private Estudante buildMockEstudante() {
+        Estudante estudante = new Estudante();
+        DadosBancarios dadosBancarios = new DadosBancarios();
+        dadosBancarios.setTipoContaBancaria(TipoContaBancaria.valueOf("CORRENTE"));
+        dadosBancarios.setDigito(1);
+        dadosBancarios.setAgencia(1234);
+        dadosBancarios.setConta(123);
+        estudante.setId(1L);
+        estudante.setNome("aluno");
+        estudante.setCurso("java");
+        estudante.setEndereco("rua");
+        estudante.setDadosBancarios(dadosBancarios);
+        return estudante;
+    }
 }
