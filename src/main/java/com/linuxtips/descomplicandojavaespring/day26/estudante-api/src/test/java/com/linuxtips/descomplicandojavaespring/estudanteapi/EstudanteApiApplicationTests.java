cea33883fbfff730474cb187dcc703aba9cfe97b
Kamila Santos <kahsantos89@gmail.com>
2023-08-22 19:56:34 -0300

    documentando aplicação

---
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day26/estudante-api/src/test/java/com/linuxtips/descomplicandojavaespring/estudanteapi/EstudanteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linuxtips.descomplicandojavaespring.day26.estudante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudanteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
